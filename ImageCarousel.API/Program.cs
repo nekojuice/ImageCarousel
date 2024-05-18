e7ecb4af1fb8bed312e75c5da140c894c069d1b0
nekojuice <joseapple7@gmail.com>
2024-05-18 23:58:53 +0800

    feat: 新增 html 貓咪圖片 carousel

--- ImageCarousel.API/Program.cs
+++ ImageCarousel.API/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+var env = builder.Environment;
 
 // Add services to the container.
 
@@ -6,6 +7,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            if (env.IsDevelopment() || env.IsEnvironment("Testing"))
+            {
+                builder.WithOrigins(
+                    "http://localhost:5500",
+                    "https://localhost:5500",
+                    "http://127.0.0.1:5500",
+                    "https://127.0.0.1:5500")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+            }
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +38,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// use static
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
